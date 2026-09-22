12eae87202d5139436d4ffba252f765c45daa5ba
Lukas Short <stockente.mail@gmail.com>
2026-09-21 22:38:15 -0700

    day 2

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-let mx;
-let my;
-let bx;
-let by;
-let lmbdown = false;
-let rmbdown = false;
+// Spritesheet
+const sheet = new Image();
+sheet.src = "spritesheet.png";
+
+let mx = false; // Mouse x
+let my = false; // Mouse y
+let bx = false; // Board x
+let by = false; // Board y
+let hvr = false; // hover position (0 - board_width * board_height)
+
+let lmbdown = false; // Left mouse button down
+let rmbdown = false; // Right mouse button down
+let mouseOverBoard  = false;
+let mouseOverFace   = false;
+let canTouchBoard   = true;
+
+const tile_width = 48;
+
+// All the difficulty presets
+let presets = [
+    [9,	 9,  10], // Beginner
+	[16, 16, 40], // Intermediate
+	[30, 16, 99], // Expert
+    [3,  3,  2 ] // Custom
+];
+
+let level = 0;
+let gamestate;
+
+let board_width;
+let board_height;
+
+let mines;
+let flags;
+let time;
+
+let face_state;
+
+let board;
+let layer;
+
+setGamestate("init");
 
 canvas.addEventListener("contextmenu", (e) => {
     e.preventDefault();
@@ -19,29 +55,65 @@ document.addEventListener("mousemove", (e) => {
     my = e.clientY - rect.top;
     bx = Math.floor((mx-30)/48);
     by = Math.floor((my-156)/48);
+
+    if(bx >= 0 && bx < board_width && by >= 0 && by < board_height) {mouseOverBoard = true}
+    else {mouseOverBoard = false}
+
+    if(mx > 207 && mx < 282 && my > 41 && my < 116) {mouseOverFace = true}
+    else {mouseOverFace = false}
+
+    hvr = by*board_width+bx;
 });
 
 document.addEventListener("mousedown", (e) => {
-    if(e.button == 0) {lmbdown = true}
-    if(e.button == 2) {rmbdown = true}
-    const t = by*board_width + bx;
-    
-    if(e.button == 2) {
-        if(layer[t] == 3) {layer[t] = 1}
-        else {layer[t] = 3}
-    } 
-})
+    switch(e.button) {
+        case 0:
+            lmbdown = true;
+            break;
+        case 2:
+            rmbdown = true;
+            if(mouseOverBoard && canTouchBoard) {
+                switch(layer[hvr]) {
+                    case 1:
+                        layer[hvr] = 3;
+                        flags --;
+                        break;
+                    case 3:
+                        layer[hvr] = 1;
+                        flags ++;
+                        break;
+                }
+            }
+        break;
+    }
+});
+
 document.addEventListener("mouseup", (e) => {
-    if(e.button == 0) {lmbdown = false}
-    if(e.button == 2) {rmbdown = false}
-})
-document.addEventListener("click", (e) => {
-    
-})
+    switch(e.button) {
+        case 0:
+            lmbdown = false;
+            if(mouseOverBoard && canTouchBoard) {
+                switch(layer[hvr]) {
+                    case 2: 
+                        if(gamestate == "init") {
+                            //Initialize game
+                            setGamestate("game");
+                        }
+                        reveal(hvr);
+                        break;
+                }
+            }
+            if(mouseOverFace) {
+                setGamestate("init");
+            }
+            break;
+        case 2:
+            rmbdown = false;
+            break;
+    }
+});
 
-const sheet = new Image();
-sheet.src = "spritesheet.png";
-
+// Locations and sizes for every sprite in the spritesheet
 let sprites = {
     d0: {x:0,y:0,w:13,h:23},
     d1: {x:13,y:0,w:13,h:23},
@@ -53,7 +125,7 @@ let sprites = {
     d7: {x:91,y:0,w:13,h:23},
     d8: {x:104,y:0,w:13,h:23},
     d9: {x:117,y:0,w:13,h:23},
-    dNeg: {x:120,y:0,w:13,h:23},
+    dNeg: {x:130,y:0,w:13,h:23},
     t0: {x:0,y:23,w:16,h:16},
     t1: {x:16,y:23,w:16,h:16},
     t2: {x:32,y:23,w:16,h:16},
@@ -76,40 +148,13 @@ let sprites = {
     eLW: {x:136,y:39,w:10,h:32},
     eXL: {x:56,y:81,w:10,h:10},
     eXR: {x:66,y:81,w:10,h:10},
-    s1: {x:0,y:55,w:26,h:26},
-    s2: {x:26,y:55,w:26,h:26},
-    s3: {x:52,y:55,w:26,h:26},
-    s4: {x:78,y:55,w:26,h:26},
-    s5: {x:104,y:55,w:26,h:26},
+    f1: {x:0,y:55,w:26,h:26},
+    f2: {x:26,y:55,w:26,h:26},
+    f3: {x:52,y:55,w:26,h:26},
+    f4: {x:78,y:55,w:26,h:26},
+    f5: {x:104,y:55,w:26,h:26},
 }
-let global_scale = 50/16;
 
-let presets = [
-    [3,  3,  2 ],
-    [9,	 9,  10],
-	[16, 16, 40],
-	[30, 16, 99]
-];
-let level = 3;
-let board_width = presets[level][0];
-let board_height = presets[level][1];
-let board_mines = presets[level][2];
-let gamestate = "idle";
-
-let flags = board_mines;
-let time = 123;
-let face_state = 1;
-canvas.width = (board_width) * 48 + 60;
-canvas.height = (board_height) * 48 + 186;
-
-const tile_width = 48;
-
-let board = new Uint8Array(board_width * board_height);
-board.fill(0);
-let layer = new Uint8Array(board_width * board_height);
-layer.fill(1);
-
-//window.requestAnimationFrame(placeMines);
 window.requestAnimationFrame(step);
 function step() {
     draw();
@@ -119,18 +164,12 @@ function step() {
 function draw() {
     ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
     
-    const t = by*board_width + bx;
-    if(layer[t] == 1 && lmbdown) {
-        layer[t] = 2; 
-        if(gamestate == "idle") {
-            gamestate = "game";
-            placeMines();
-            collapseEmpty(by*board_width+bx);
-        }
-        if(board[t] == 0) {collapseEmpty(t)}
+    if(layer[hvr] == 1 && lmbdown && mouseOverBoard && canTouchBoard) {
+        layer[hvr] = 2;
     } //reveal
-    if(layer[t] == 2 && !lmbdown) {layer[t] = 0;} //cancel reveal
-    //if(layer[t] == 1 && rmbdown) {layer[t] = 3;} //flag
+    if(layer[hvr] == 2 && !lmbdown) {layer[hvr] = 0;} //cancel reveal
+
+    
 
     //Draw header
     const hx = 0;
@@ -145,22 +184,45 @@ function draw() {
     for(let w = 0; w < board_width; w ++) {
         hqueue.push({data:sprites["eW"],x:hx+30+(w*48),y:hy,w:48,h:30});
     }
-    //Scores
 
-    let strFlags = flags.toString();
+    // Face State
+    if(gamestate == "lose") {face_state = 4}
+    if(gamestate == "win") {face_state = 5}
+    if(lmbdown && mouseOverFace) {face_state = 2;}
+    if(lmbdown && mouseOverBoard && canTouchBoard) {face_state = 3}
+    else if(face_state == 3 && canTouchBoard) {face_state = 1}
+    //Display Face
+    console.log(face_state);
+    hqueue.push({data:sprites[`f${face_state}`],x:hx+30+(board_width/2*48)-39,y:hy+40,w:78,h:78});
+    
+    //Display Flags left
+    let strFlags = Math.abs(flags).toString();
 
-    if(flags < 100) {strFlags = `0${strFlags}`}
+    if(Math.abs(flags) < 100) {strFlags = `0${strFlags}`}
+    if(Math.abs(flags) < 10) {strFlags = `0${strFlags}`}
+    if(flags < 0) {strFlags = `-${strFlags[1]}${strFlags[2]}`}
     for(let i = 0; i < 3; i ++) {
-        hqueue.push({data:sprites[`d${strFlags[i]}`],x:hx+48+(i*39),y:hy+45,w:39,h:69});
+        let newSprite = {data:sprites[`d${strFlags[i]}`],x:hx+48+(i*39),y:hy+45,w:39,h:69}
+        if(strFlags[i] == "-") {newSprite.data = sprites['dNeg']}
+        hqueue.push(newSprite);
     }
+
+    //Display Time left
     let strTime = time.toString();
+    if(time < 100) {strTime = `0${strTime}`}
+    if(time < 10) {strTime = `0${strTime}`}
     for(let i = 2; i >= 0; i --) {
-        hqueue.push({data:sprites[`d${strTime[i]}`],x:hx+30-135+(board_width*48)+(i*39),y:hy+45,w:39,h:69});
+        let newSprite = {data:sprites[`d${strTime[i]}`],x:hx+30-135+(board_width*48)+(i*39),y:hy+45,w:39,h:69};
+        hqueue.push(newSprite);
     }
-    hqueue.push({data:sprites[`s${face_state}`],x:hx+30+(board_width/2*48)-39,y:hy+40,w:78,h:78});
+
+    
+
+    //Draw all sprites in header queue
     for(let a of hqueue) {
         ctx.drawImage(sheet, a.data.x, a.data.y, a.data.w, a.data.h, hx+a.x, hy+a.y, a.w, a.h);
     }
+
     //Draw tiles
     for(let i = 0; i < board_width*board_height; i++) {
         let d = board[i];
@@ -171,6 +233,8 @@ function draw() {
         if(layer[i] == 1) {sheet_index = "tOff"}
         if(layer[i] == 2) {sheet_index = "t0"; if(i != by*board_width+bx) {layer[i] = 1}}
         if(layer[i] == 3) {sheet_index = "tFlag"}
+        if(layer[i] == 4) {sheet_index = "tBomb1"}
+        if(layer[i] == 5) {sheet_index = "tBomb2"}
 
         let data = sprites[sheet_index];
         ctx.imageSmoothingEnabled = false;
@@ -194,8 +258,47 @@ function draw() {
     }
 }
 
+function setGamestate(state) {
+    gamestate = state;
+    switch(gamestate) {
+        case "init":
+            canTouchBoard = true;
+
+            board_width = presets[level][0];
+            board_height = presets[level][1];
+            canvas.width = (board_width) * 48 + 60;
+            canvas.height = (board_height) * 48 + 186;
+
+            mines = presets[level][2];
+            flags = mines;
+            time = 0;
+
+            face_state = 1;
+
+            board = new Uint8Array(board_width * board_height);
+            board.fill(0);
+            layer = new Uint8Array(board_width * board_height);
+            layer.fill(1);
+            break;
+        case "game":
+            placeMines();
+            break;
+        case "lose":
+            
+            canTouchBoard = false;
+            for(let i = 0; i < board_width * board_height; i ++) {
+                if(board[i] > 8 && layer[i] != 3) {layer[i] = 0}
+                if(board[i] <= 8 && layer[i] == 3) {layer[i] = 5}
+            }
+            layer[hvr] = 4;
+            break;
+        case "win":
+            canTouchBoard = false;
+            break;
+    }
+}
+
 function placeMines() {
-    console.log("place")
     const t = by*board_width + bx;
     board[t] = 0;
     let safe_tiles = [
@@ -210,7 +313,7 @@ function placeMines() {
         t-1-board_width
     ];
     let placed = 0;
-    for(let i = 0; i < board_mines; i ++) {
+    for(let i = 0; i < mines; i ++) {
         let found = false;
         let r;
         while(!found) {
@@ -219,13 +322,11 @@ function placeMines() {
             for(let s of safe_tiles) {
                 if(r == s) {found = false}
             }
-            //console.log(r, s, found)
         }
         board[r] = 9;
         incrNeighbors(r);
         placed ++;
     }
-    console.log(placed);
 }
 
 function incrNeighbors(p) {
@@ -267,4 +368,10 @@ function collapseEmpty(p) {
         }
         closed.add(current);
     }
+}
+
+function reveal(p) {
+    layer[p] = 0;
+    if(board[p] == 0) {collapseEmpty(p);}
+    if(board[p] > 8) {setGamestate("lose");}
 }
